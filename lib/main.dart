b0e12b342fc0126bf867f4a6155b56ff7e0ae891
FlutLab.io <>
2022-05-02 18:59:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         title: 'Aprendiendo GridView',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.green,
+          primarySwatch: Colors.red,
         ),
         home: MyHomePage());
   }
